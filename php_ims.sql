ef8dc1466c1a914d3934390e87e82a4f6c3ec65a
Tanvirul <anmtanvir872@gmail.com>
2022-01-12 18:05:05 +0600

    change

--- php_ims.sql
+++ php_ims.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 09, 2022 at 06:51 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 7.3.28
+-- Generation Time: Jan 12, 2022 at 01:04 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,97 @@ SET time_zone = "+00:00";
 --
 -- Database: `php_ims`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company_name`
+--
+
+CREATE TABLE `company_name` (
+  `id` int(5) NOT NULL,
+  `company_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `company_name`
+--
+
+INSERT INTO `company_name` (`id`, `company_name`) VALUES
+(16, 'Tata'),
+(17, 'Bajaj'),
+(18, 'Choruighor');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `party_info`
+--
+
+CREATE TABLE `party_info` (
+  `id` int(5) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `businessname` varchar(100) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `address` varchar(500) NOT NULL,
+  `city` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `party_info`
+--
+
+INSERT INTO `party_info` (`id`, `firstname`, `lastname`, `businessname`, `contact`, `address`, `city`) VALUES
+(1, 'tanvir ', 'tasneem', 'choruighor', '02929333322', 'sdsadasdd', 'dhaka '),
+(3, 'tanvir', 'Karim', 'কিরন্মিয়া', '01966927688', 'Bamoil ,demra ,dhaka\r\n১২, ৪/এ', 'মিরপুর'),
+(4, 'Rahela updated', 'Khatun updated', 'rahelar dokan  updated', 'dfdfddfdfdf updated', 'Mirpur 10  updated', 'dhaka  updated');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(5) NOT NULL,
+  `company_name` varchar(100) NOT NULL,
+  `product_name` varchar(100) NOT NULL,
+  `unit` varchar(50) NOT NULL,
+  `packing_size` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `company_name`, `product_name`, `unit`, `packing_size`) VALUES
+(1, 'Bajaj', 'Product One updated', 'uni-1', 'packing-size-1 updat'),
+(2, 'Tata', 'Product Two upp', 'kg', 'packing-size-1 upp'),
+(5, 'Tata', 'ssdsd', 's', 'sdsd'),
+(6, 'Choruighor', 'sdsdf', 'kg', 'fgfg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unit`
+--
+
+CREATE TABLE `unit` (
+  `id` int(5) NOT NULL,
+  `unit` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `unit`
+--
+
+INSERT INTO `unit` (`id`, `unit`) VALUES
+(3, 's'),
+(6, 'kg'),
+(22, 'gm'),
+(23, 'uni-1'),
+(24, 'unit-2');
 
 -- --------------------------------------------------------
 
@@ -46,12 +137,35 @@ INSERT INTO `user_registration` (`id`, `firstname`, `lastname`, `username`, `pas
 (2, 'admin', 'admin', 'admin', 'admin', 'admin', 'active'),
 (3, 'tanvir ', 'karimul ', 'tanvirul ', '1234567', '', 'active'),
 (4, 'Tanvirul', 'karim ', 'tanvirrrr ', '123', 'User', 'active'),
-(5, 'user 1 ', 'user lastname ', 'user-one ', '12345', 'Admin', 'active'),
-(6, 'Rahela ', 'khatun ', 'rahela ', '123', 'User', 'active');
+(11, 'dsdsds ssss', 'sdsdsd  ssss', 'adminsdsdsds', '12345ssds', 'user', 'inactive');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `company_name`
+--
+ALTER TABLE `company_name`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `party_info`
+--
+ALTER TABLE `party_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `unit`
+--
+ALTER TABLE `unit`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user_registration`
@@ -64,10 +178,34 @@ ALTER TABLE `user_registration`
 --
 
 --
+-- AUTO_INCREMENT for table `company_name`
+--
+ALTER TABLE `company_name`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `party_info`
+--
+ALTER TABLE `party_info`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `unit`
+--
+ALTER TABLE `unit`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
 -- AUTO_INCREMENT for table `user_registration`
 --
 ALTER TABLE `user_registration`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
