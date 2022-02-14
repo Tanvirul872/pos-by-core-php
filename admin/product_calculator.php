725743decc8615cfc88f64be1f61bed587ab77cd
Tanvirul872 <64800533+Tanvirul872@users.noreply.github.com>
2022-02-15 00:50:42 +0600

    change

--- admin/product_calculator.php
+++ admin/product_calculator.php
@@ -1,12 +1,23 @@
+<?php  
+session_start();  
+if(!isset($_SESSION["admin"])){
+    ?> 
+   <script type="text/javascript">
+    window.location = "index.php" ; 
+   </script>
+   <?php
+}
+?>
+
 <?php
 include "header.php";
 include "../user/connection.php";
 ?>
 
+
     <!-- partial -->
     <div class="main-panel">
         <div class="content-wrapper">
-
 
             <div class="row">
                 add new product ...
@@ -15,19 +26,30 @@ include "../user/connection.php";
                         <div class="card-body">
                             <p class="card-description"> Add new product </p>
                             <form class="forms-sample" action="" method="post" name="form1">
+                              
+                               <div class="form-group">
+                                    <label> Product name </label>
+                                    <input type="text" class="form-control" name="product_name" placeholder="Product name">
+                                </div>
                                 <div class="form-group">
+                                    <label> Product code </label>
+                                    <input type="text" class="form-control" name="product_code" placeholder="Product Code">
+                                </div>
+
+                               <div class="form-group">
                                     <label> Buying Price </label>
                                     <input type="text" class="form-control" name="buying_price" placeholder="Buying Price">
                                 </div>
 
-                                <div class="form-group">
+                                <!-- <div class="form-group">
                                     <label> Shpping Cost </label>
                                     <input type="text" class="form-control" name="shipping_cost" placeholder="Shipping Cost">
-                                </div>
-                                <div class="form-group">
+                                </div> -->
+
+                                <!-- <div class="form-group">
                                     <label> Staff Cost </label>
                                     <input type="text" class="form-control" name="staff_cost" placeholder="Staff Cost">
-                                </div>
+                                </div> -->
 
                                 <div class="form-group">
                                     <label> Packaging Cost </label>
@@ -37,14 +59,19 @@ include "../user/connection.php";
                                     <label> Weight Cost </label>
                                     <input type="text" class="form-control" name="weight_cost" placeholder="Weight Cost">
                                 </div>
-                                <div class="form-group">
+                                <!-- <div class="form-group">
                                     <label> COD Charge </label>
                                     <input type="text" class="form-control" name="cod_cost" placeholder="COD Cost">
-                                </div>
-                                <div class="form-group">
+                                </div> -->
+                                <!-- <div class="form-group">
                                     <label> Bkash Charge </label>
                                     <input type="text" class="form-control" name="bkash_cost" placeholder="Bkash Charge">
-                                </div>
+                                </div> -->
+
+                                <!-- <div class="form-group">
+                                    <label> Total Charge </label>
+                                    <input type="text" class="form-control" name="total_cost" placeholder="Total cost">
+                                </div> -->
 
 
                                 <button type="submit" class="btn btn-primary mr-2" name="submit1">Submit</button>
@@ -67,78 +94,132 @@ include "../user/connection.php";
 
             </div>
 
-<!--            --><?php
-//            if(isset($_POST['submit1'])){
-//                $count =0;
-//                $res = mysqli_query($link,"select * from company_name where company_name='$_POST[company_name]'");
-//                $count=mysqli_num_rows($res);
-//
-//                if($count > 0){ ?>
-<!--                    <script type="text/javascript">-->
-<!---->
-<!--                        document.getElementById("error").style.display="block"-->
-<!--                        document.getElementById("success").style.display="none"-->
-<!---->
-<!--                    </script>-->
-<!--                --><?php //}else{
-//
-//                mysqli_query($link,"INSERT INTO company_name VALUES (NULL,'$_POST[company_name]')") or die (mysqli_error($link));
-//                ?>
-<!--                    <script type="text/javascript">-->
-<!--                        document.getElementById("error").style.display="none"-->
-<!--                        document.getElementById("success").style.display="block"-->
-<!--                        setTimeout(function(){-->
-<!--                            window.location.href= window.location.href;-->
-<!--                        },3000)-->
-<!--                    </script>-->
-<!---->
-<!--                --><?php //}  }  ?>
+            <?php
+            if(isset($_POST['submit1'])){
+                $product_name = $_POST['product_name']; 
+                $product_code = $_POST['product_code']; 
+                $buying_price = $_POST['buying_price']; 
+                $shipping_cost =  $buying_price*0.1;                                      
+                $staff_cost =$buying_price*0.05; 
+                $packaging_cost = $_POST['packaging_cost']; 
+                $weight_cost = $_POST['weight_cost']*0.02; 
+                $cod_cost = $buying_price*0.01; 
+                $bkash_cost = $buying_price*0.018; 
+                $total_cost = $buying_price+$shipping_cost+$staff_cost+$packaging_cost+$weight_cost+$cod_cost+$bkash_cost ;
+                $total_cost_1 = $total_cost*0.3 ;        
+                $final_cost = $total_cost+$total_cost_1 ;        
+
+
+                // echo "<pre>";
+                // echo 'Product name ='.$product_name;
+                // echo '<br>'; 
+                // echo 'Product code ='.$product_code;
+                // echo '<br>';
+                // echo 'buying price ='.$buying_price;
+                // echo '<br>'; 
+                // echo 'shipping_cost ='.$shipping_cost;
+                // echo '<br>'; 
+                // echo 'staff_cost ='.$staff_cost;
+                // echo '<br>'; 
+                // echo 'packaging_cost ='.$packaging_cost;
+                // echo '<br>'; 
+                // echo 'weight_cost ='.$weight_cost;
+                // echo '<br>'; 
+                // echo 'cod_cost ='.$cod_cost;
+                // echo '<br>'; 
+                // echo 'bkash_cost ='.$bkash_cost;
+                // echo '<br>'; 
+                // echo 'total_cost ='.$total_cost;
+                // echo '<br>'; 
+                // echo '30% profit ='.$total_cost_1;
+                // echo '<br>'; 
+                // echo 'final_cost ='.$final_cost;
+                // echo '<br>'; 
+                // echo "</pre>";
+
+                // exit; 
+
+
+                mysqli_query($link,"INSERT INTO `product_calculator`(`id`, `product_name`, `product_code`, `buying_price`, `shipping_cost`, `staff_cost`, `packaging_cost`, `weight_cost`, `cod_cost`, `bkash_cost`, `total_cost`) VALUES (NULL,'$product_name','$product_code','$buying_price','$shipping_cost','$staff_cost','$packaging_cost','$weight_cost','$cod_cost','$bkash_cost','$total_cost')") or die (mysqli_error($link));
+
+                            
+            ?>
+
+                <!-- <script>
+                    alert('hello'); 
+                </script> -->
+                    <script type="text/javascript">
+                        document.getElementById("error").style.display="none"
+                        document.getElementById("success").style.display="block"
+                        setTimeout(function(){
+                            window.location.href= window.location.href;
+                        },3000)
+                    </script>
+
+                <?php }   ?>
         </div>
 
+        <div class="col-lg-12 grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">All Product Calculation </h4>
+                    <div class="table-responsive">
+                        <table class="table table-dark">
+                            <thead>
+                            <tr>
+                                <th> Id </th>
+                                <th> Product name </th>
+                                <th> product code  </th>
+                                <th> buying price  </th>
+                                <th> shipping cost  </th>
+                                <th> staff  cost  </th>
+                                <th> packaging cost  </th>
+                                <th> weight cost  </th>
+                                <th> cod cost  </th>
+                                <th> bkash cost  </th>
+                                <th> total cost </th>
+                                <th> Edit </th>
+                                <th> Delete </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                            $res=mysqli_query($link,"select * from product_calculator");
 
-<!---->
-<!--        <div class="col-lg-12 grid-margin stretch-card">-->
-<!--            <div class="card">-->
-<!--                <div class="card-body">-->
-<!--                    <h4 class="card-title">All Company </h4>-->
-<!--                    <div class="table-responsive">-->
-<!--                        <table class="table table-dark">-->
-<!--                            <thead>-->
-<!--                            <tr>-->
-<!--                                <th> Id </th>-->
-<!--                                <th> Company name </th>-->
-<!--                                <th> Edit </th>-->
-<!--                                <th> Delete </th>-->
-<!--                            </tr>-->
-<!--                            </thead>-->
-<!--                            <tbody>-->
-<!---->
-<!---->
-<!--                            --><?php
-//                            $res=mysqli_query($link,"select * from company_name");
-//                            //                            $row=mysqli_fetch_assoc($res);
-//
-//                            //   echo "<pre>";
-//                            //   print_r($row);
-//                            //   echo "</pre>";
-//
-//                            while($row=mysqli_fetch_assoc($res)){ ?>
-<!--                                <tr>-->
-<!--                                    <td> --><?//= $row['id'];?><!-- </td>-->
-<!--                                    <td> --><?//= $row['company_name'];?><!-- </td>-->
-<!--                                    <td> <a href="edit_company.php?id=--><?php //echo $row['id'];?><!--"> Edit </a></td>-->
-<!--                                    <td> <a href="delete_company.php?id=--><?php //echo $row['id'];?><!--"> Delete </a></td>-->
-<!--                                </tr>-->
-<!---->
-<!--                            --><?php //}  ?>
-<!---->
-<!---->
-<!--                            </tbody>-->
-<!--                        </table>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
+                            //    $row=mysqli_fetch_assoc($res);
+                            //   echo "<pre>";
+                            //   print_r($row);
+                            //   echo "</pre>";
+
+                            while($row=mysqli_fetch_assoc($res)){ ?>
+
+                                <tr>
+                                    <td> <?= $row['id'];?> </td>
+                                    <td> <?= $row['product_name'];?> </td>
+                                    <td> <?= $row['product_code'];?> </td>
+                                    <td> <?= $row['buying_price'];?> </td>
+                                    <td> <?= $row['shipping_cost'];?> </td>
+                                    <td> <?= $row['staff_cost'];?> </td>
+                                    <td> <?= $row['packaging_cost'];?> </td>
+                                    <td> <?= $row['weight_cost'];?> </td>
+
+                                    <td> <?= $row['cod_cost'];?> </td>
+                                    <td> <?= $row['bkash_cost'];?> </td>
+                                    <td> <?= $row['total_cost'];?> </td>
+
+                                    <td> <a href="edit_calculation.php?id=<?php echo $row['id'];?>"> Edit </a></td>
+                                    <td> <a href="delete_calculation.php?id=<?php echo $row['id'];?>"> Delete </a></td>
+                                </tr>
+
+                            <?php }  ?>
+
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
 
--- php_ims.sql
+++ php_ims.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 31, 2022 at 06:11 PM
+-- Generation Time: Feb 14, 2022 at 07:49 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 7.3.28
 
@@ -38,17 +38,6 @@ CREATE TABLE `billing_details` (
   `qty` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `billing_details`
---
-
-INSERT INTO `billing_details` (`id`, `bill_id`, `product_company`, `product_name`, `product_unit`, `packing_size`, `price`, `qty`) VALUES
-(1, '5', 'Tata', 'Product Two upp', 'kg', 'packing-size-1 upp', '123', '4'),
-(2, '5', 'Bajaj', 'Product One updated', 'uni-1', 'packing-size-1 updat', '456', '10'),
-(3, '6', 'Tata', 'Product Two upp', 'kg', 'packing-size-1 upp', '123', '2'),
-(4, '7', 'Bajaj', 'Product One updated', 'uni-1', 'packing-size-1 updat', '456', '10'),
-(5, '8', 'Tata', 'Product Two upp', 'kg', 'packing-size-1 upp', '123', '7');
-
 -- --------------------------------------------------------
 
 --
@@ -60,22 +49,24 @@ CREATE TABLE `billing_header` (
   `full_name` varchar(100) NOT NULL,
   `bill_type` varchar(50) NOT NULL,
   `date` date NOT NULL,
-  `bill_no` varchar(10) NOT NULL
+  `bill_no` varchar(10) NOT NULL,
+  `username` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `billing_header`
 --
 
-INSERT INTO `billing_header` (`id`, `full_name`, `bill_type`, `date`, `bill_no`) VALUES
-(1, '', 'bill 1', '0000-00-00', '00001'),
-(2, '', 'bill 1', '0000-00-00', '00001'),
-(3, 'tanvirul karim', 'bill 1', '2022-01-23', '00002'),
-(4, '', 'bill 1', '0000-00-00', '00003'),
-(5, 'tanvirul karim', 'bill 1', '2022-01-08', '00005'),
-(6, '', 'bill 1', '0000-00-00', '00006'),
-(7, '', 'bill 1', '0000-00-00', '00007'),
-(8, 'abcd', 'bill 1', '2022-01-31', '00008');
+INSERT INTO `billing_header` (`id`, `full_name`, `bill_type`, `date`, `bill_no`, `username`) VALUES
+(1, '', 'bill 1', '0000-00-00', '00001', 'admin'),
+(2, '', 'bill 1', '0000-00-00', '00001', 'admin'),
+(3, 'tanvirul karim', 'bill 1', '2022-01-23', '00002', 'admin'),
+(4, '', 'bill 1', '0000-00-00', '00003', 'admin'),
+(5, 'tanvirul karim', 'bill 1', '2022-01-08', '00005', 'admin'),
+(6, '', 'bill 1', '0000-00-00', '00006', 'admin'),
+(7, '', 'bill 1', '0000-00-00', '00007', 'admin'),
+(8, 'abcd', 'bill 1', '2022-01-31', '00008', 'admin'),
+(9, '', 'bill 1', '0000-00-00', '00009', 'admin');
 
 -- --------------------------------------------------------
 
@@ -152,6 +143,34 @@ INSERT INTO `products` (`id`, `company_name`, `product_name`, `unit`, `packing_s
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `product_calculator`
+--
+
+CREATE TABLE `product_calculator` (
+  `id` int(5) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `product_code` varchar(255) NOT NULL,
+  `buying_price` varchar(255) NOT NULL,
+  `shipping_cost` varchar(255) NOT NULL,
+  `staff_cost` varchar(255) NOT NULL,
+  `packaging_cost` varchar(255) NOT NULL,
+  `weight_cost` varchar(255) NOT NULL,
+  `cod_cost` varchar(255) NOT NULL,
+  `bkash_cost` varchar(255) NOT NULL,
+  `total_cost` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `product_calculator`
+--
+
+INSERT INTO `product_calculator` (`id`, `product_name`, `product_code`, `buying_price`, `shipping_cost`, `staff_cost`, `packaging_cost`, `weight_cost`, `cod_cost`, `bkash_cost`, `total_cost`) VALUES
+(1, 'Product One', 'chw112', '100', '10', '5', '5', '0.2', '1', '1.8', '123'),
+(2, 'Product One updated', 'chw112', '100', '10', '5', '10', '4', '1', '1.8', '131.8');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `purchase_master`
 --
 
@@ -165,19 +184,58 @@ CREATE TABLE `purchase_master` (
   `price` varchar(10) NOT NULL,
   `party_name` varchar(100) NOT NULL,
   `purchase_type` varchar(100) NOT NULL,
-  `expiry_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `expiry_date` date NOT NULL,
+  `purchase_date` date DEFAULT NULL,
+  `username` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `purchase_master`
 --
 
-INSERT INTO `purchase_master` (`id`, `company_name`, `product_name`, `unit`, `packing_size`, `quantity`, `price`, `party_name`, `purchase_type`, `expiry_date`) VALUES
-(15, 'Bajaj', 'Product One updated', 'uni-1', 'packing-size-1 updat', '123', '1234', 'choruighor', 'Debit', '2022-10-09 18:00:00'),
-(16, 'Choruighor', 'sdsdf', 'kg', 'fgfg', '34', '678', 'rahelar dokan updated', 'Credit', '2021-10-09 18:00:00'),
-(17, 'Tata', 'Product Two upp', 'kg', 'packing-size-1 upp', '12', '123', 'choruighor', 'Debit', '2022-10-09 18:00:00'),
-(18, 'Tata', 'Product Two upp', 'kg', 'packing-size-1 upp', '12', '123', 'choruighor', 'Debit', '2022-10-09 18:00:00'),
-(19, 'Bajaj', 'Product One updated', 'uni-1', 'packing-size-1 updat', '23', '45', 'কিরন্মিয়া', 'Debit', '2022-10-09 18:00:00');
+INSERT INTO `purchase_master` (`id`, `company_name`, `product_name`, `unit`, `packing_size`, `quantity`, `price`, `party_name`, `purchase_type`, `expiry_date`, `purchase_date`, `username`) VALUES
+(15, 'Bajaj', 'Product One updated', 'uni-1', 'packing-size-1 updat', '123', '1234', 'choruighor', 'Debit', '2022-02-06', '0000-00-00', 'admin'),
+(16, 'Choruighor', 'sdsdf', 'kg', 'fgfg', '34', '678', 'rahelar dokan updated', 'Credit', '2022-02-06', '0000-00-00', 'admin'),
+(17, 'Tata', 'Product Two upp', 'kg', 'packing-size-1 upp', '12', '123', 'choruighor', 'Debit', '2022-02-06', '0000-00-00', 'admin'),
+(18, 'Tata', 'Product Two upp', 'kg', 'packing-size-1 upp', '12', '123', 'choruighor', 'Debit', '2022-02-06', '0000-00-00', 'admin'),
+(19, 'Bajaj', 'Product One updated', 'uni-1', 'packing-size-1 updat', '23', '45', 'কিরন্মিয়া', 'Debit', '2022-02-06', '0000-00-00', 'admin'),
+(23, 'Bajaj', 'Product One updated', 'uni-1', 'packing-size-1 updat', '445', '5665', 'কিরন্মিয়া', 'Debit', '2022-02-23', '2022-02-01', ''),
+(24, '', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `return_products`
+--
+
+CREATE TABLE `return_products` (
+  `retuen_by` varchar(50) NOT NULL,
+  `id` int(5) NOT NULL,
+  `bill_no` int(10) NOT NULL,
+  `return_date` varchar(15) NOT NULL,
+  `product_company` varchar(50) NOT NULL,
+  `product_name` varchar(50) NOT NULL,
+  `product_unit` varchar(20) NOT NULL,
+  `product_size` varchar(20) NOT NULL,
+  `product_price` varchar(10) NOT NULL,
+  `product_qty` varchar(10) NOT NULL,
+  `total` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `return_products`
+--
+
+INSERT INTO `return_products` (`retuen_by`, `id`, `bill_no`, `return_date`, `product_company`, `product_name`, `product_unit`, `product_size`, `product_price`, `product_qty`, `total`) VALUES
+('admin', 1, 5, '2022-01-31', 'Tata', 'Product Two upp', 'kg', 'packing-size-1 upp', '123', '4', '492'),
+('admin', 2, 5, '2022-01-31', 'Bajaj', 'Product One updated', 'uni-1', 'packing-size-1 updat', '456', '10', '4560'),
+('admin', 3, 6, '2022-01-31', 'Tata', 'Product Two upp', 'kg', 'packing-size-1 upp', '123', '2', '246'),
+('admin', 4, 6, '2022-01-31', 'Tata', 'Product Two upp', 'kg', 'packing-size-1 upp', '123', '2', '246'),
+('admin', 5, 7, '2022-01-31', 'Bajaj', 'Product One updated', 'uni-1', 'packing-size-1 updat', '456', '10', '4560'),
+('admin', 6, 8, '2022-01-31', 'Tata', 'Product Two upp', 'kg', 'packing-size-1 upp', '123', '7', '861'),
+('admin', 7, 0, '2022-01-31', '', '', '', '', '', '', '0'),
+('admin', 8, 9, '2022-01-31', 'Tata', 'Product Two upp', 'kg', 'packing-size-1 upp', '123', '3', '369'),
+('admin', 9, 0, '2022-01-31', '', '', '', '', '', '', '0');
 
 -- --------------------------------------------------------
 
@@ -200,8 +258,9 @@ CREATE TABLE `stock_master` (
 --
 
 INSERT INTO `stock_master` (`id`, `product_company`, `product_name`, `product_unit`, `packing_size`, `product_qty`, `product_selling_price`) VALUES
-(4, 'Tata', 'Product Two upp', 'kg', 'packing-size-1 upp', '13', '123'),
-(5, 'Bajaj', 'Product One updated', 'uni-1', 'packing-size-1 updat', '10', '456');
+(4, 'Tata', 'Product Two upp', 'kg', 'packing-size-1 upp', '85', '123'),
+(5, 'Bajaj', 'Product One updated', 'uni-1', 'packing-size-1 updat', '6142', '456'),
+(6, '', '', '', '', '', '0');
 
 -- --------------------------------------------------------
 
@@ -287,9 +346,21 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `product_calculator`
+--
+ALTER TABLE `product_calculator`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `purchase_master`
 --
 ALTER TABLE `purchase_master`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `return_products`
+--
+ALTER TABLE `return_products`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -318,13 +389,13 @@ ALTER TABLE `user_registration`
 -- AUTO_INCREMENT for table `billing_details`
 --
 ALTER TABLE `billing_details`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `billing_header`
 --
 ALTER TABLE `billing_header`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `company_name`
@@ -345,16 +416,28 @@ ALTER TABLE `products`
   MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
+-- AUTO_INCREMENT for table `product_calculator`
+--
+ALTER TABLE `product_calculator`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `purchase_master`
 --
 ALTER TABLE `purchase_master`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `return_products`
+--
+ALTER TABLE `return_products`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `stock_master`
 --
 ALTER TABLE `stock_master`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `unit`
